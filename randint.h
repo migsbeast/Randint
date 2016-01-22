8e7dafaced41142223c76ad7349ee27fc0ea8f55
migsbeast <marodr705@tamu.edu>
2016-01-22 13:02:33 -0600

    Create randint.h

---
+++ randint.h
@@ -0,0 +1,14 @@
+//
+//  randint.h
+//  
+//
+//  Created by Miguel Rodriguez on 10/9/14.
+//
+//
+
+#ifndef ____randint__
+#define ____randint__
+
+#include <stdio.h>
+
+#endif /* defined(____randint__) */
